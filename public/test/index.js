6eceb9584976314d471b08f75c8dde2a723f4ea0
brian3t <ngxtri@gmail.com>
2019-06-14 23:28:08 -0700

    intro to vue

--- public/test/index.js
+++ public/test/index.js
@@ -1,12 +1,37 @@
+
 var app = new Vue({
-    el: '#app',
-    data: {
-        message: 'Hello Vue!'
-    }
+  el: '#shopping-list',
+  data: {
+    header: 'vue is ready to party',
+    newitem: '',
+    items: [
+      {id: 0, text: 'Vegetables'},
+      {id: 1, text: 'Cheese'},
+      {id: 2, text: 'Whatever else humans are supposed to eat'}
+    ]
+  }
 });
-var app2 = new Vue({
-    el: '#app-2',
-    data: {
-        message: 'You loaded this page on ' + new Date().toLocaleString()
-    }
-});
+/*
+Vue.component('todo-item', {
+  // The todo-item component now accepts a
+  // "prop", which is like a custom attribute.
+  // This prop is called todo.
+  props: ['todo'],
+  created: function () {
+    console.log(`im created`);
+  },
+  template: '<li>{{todo.text}}</li>',
+})
+
+var app7 = new Vue({
+  el: '#app-7',
+  data: {
+    groceryList: [
+      {id: 0, text: 'Vegetables'},
+      {id: 1, text: 'Cheese'},
+      {id: 2, text: 'Whatever else humans are supposed to eat'}
+    ]
+  },
+})
+
+*/
